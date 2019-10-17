d7d8c20ec03316b6ba9425ee123166827fdbe52a
eb88ibyw <soroosh.arasteh@fau.de>
2019-10-17 16:20:13 +0200

    1.1 Exercise Skeleton

---
+++ Assignment 0/pattern.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Checker:
+
+    def __init__(self):
+        return
+
+
+
+
+
+
+
+
+
+class Spectrum:
+
+    def __init__(self):
+        return
+
+
+
+
+
+class Circle:
+
+    def __init__(self):
+        return
