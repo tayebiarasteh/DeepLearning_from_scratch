88d1b0c262429e720e014a8171e662137aae5168
eb88ibyw <soroosh.arasteh@fau.de>
2019-10-17 20:36:32 +0200

    1.2 Checkerboard [Failed the test] + files added

---
+++ Assignment 0/main.py
@@ -0,0 +1,14 @@
+from pattern import *
+from NumpyTests import *
+import pdb
+
+
+'''
+Checker
+'''
+sample_checker = Checker(8,2)
+# pdb.set_trace()
+sample_checker.draw()
+sample_checker.show()
+
+
--- Assignment 0/pattern.py
+++ Assignment 0/pattern.py
@@ -2,29 +2,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-class Checker:
+class Checker():
+
+    def __init__(self, resolution, tile_size):
+        '''
+        :param resolution: an integer that defines the number of pixels in each dimension
+        :param tile_size: an integer that defines the number of pixel an individual tile has in each dimension.
+        '''
+        self.resolution = resolution
+        self.tile_size = tile_size
+        self.output = []
+
+    def draw(self):
+        assert self.resolution % 2*(self.tile_size) == 0
+        # number of the tiles
+        half_tiles_num = int(self.resolution/(2*self.tile_size))
+        self.output = np.array([[0,1],[1,0]])
+        self.output = np.tile(self.output, (half_tiles_num, half_tiles_num))
+        return self.output
+
+    def show(self):
+        plt.imshow(self.output)
+        plt.show()
+
+
+
+
+
+
+
+class Spectrum():
 
     def __init__(self):
-        return
+        pass
 
 
 
 
-
-
-
-
-
-class Spectrum:
+class Circle():
 
     def __init__(self):
-        return
-
-
-
-
-
-class Circle:
-
-    def __init__(self):
-        return
+        pass
