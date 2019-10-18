e9ba9df143587be9a27fc368415504c05f3ba800
eb88ibyw <soroosh.arasteh@fau.de>
2019-10-18 20:51:59 +0200

    Initial commit for class_name() & new()

--- Assignment 0/generator.py
+++ Assignment 0/generator.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 # This next function returns the next generated object. In our case it returns the input of a neural network each time it gets called.
 # This input consists of a batch of images and its corresponding labels.
 class ImageGenerator:
-    def __init__(self, file_path, label_path, batch_size, image_size, rotation=False, mirroring=False, shuffle=False):
+    def __init__(self, file_path, json_path, batch_size, image_size, rotation=False, mirroring=False, shuffle=False):
         # Define all members of your generator class object as global members here.
         # These need to include:
         # the batch size
@@ -21,6 +21,11 @@ class ImageGenerator:
         self.class_dict = {0: 'airplane', 1: 'automobile', 2: 'bird', 3: 'cat', 4: 'deer', 5: 'dog', 6: 'frog',
                            7: 'horse', 8: 'ship', 9: 'truck'}
         #TODO: implement constructor
+        self.path = (file_path, json_path)
+        self.batch_size = batch_size
+        self.image_size = image_size
+        self.shuffle = shuffle
+        self.counter = 0    # shows the number of times next() has been called for each object of the class.
 
     def next(self):
         # This function creates a batch of images and corresponding labels and returns it.
@@ -28,7 +33,32 @@ class ImageGenerator:
         # Think about how to handle such cases
         #TODO: implement next method
 
-        return batch, labels
+        images = [] # a batch (list) of images
+        labels = [] # the corresponding labels
+
+        with open(self.path[1]) as data_file:
+            label_file = json.load(data_file)
+        all_images_indices = np.arange(len(label_file)) # indices of all the images in the dataset in a numpy array.
+        if self.shuffle:
+            np.random.shuffle(all_images_indices)
+
+        '''If the last batch is smaller than the others, 
+        complete that batch by reusing images from the beginning of your training data set:'''
+        if (self.counter+1)*self.batch_size > len(label_file):
+            offset = (self.counter+1)*self.batch_size - len(label_file)
+            chosen_batch = all_images_indices[
+                           self.counter * self.batch_size :len(label_file)]
+            chosen_batch.append(all_images_indices[0:offset])
+            self.counter = -1   # at the end of the method with +1, it becomes zero and we basically reset our counter.
+        else:
+            chosen_batch = all_images_indices[self.counter*self.batch_size:(self.counter+1)*self.batch_size]
+
+        for i in chosen_batch:
+            images.append(np.load(os.path.join(self.path[0], str(i) + '.npy')))
+            labels.append(label_file[str(i)])
+
+        self.counter += 1
+        return (images, labels)
 
     def augment(self,img):
         # this function takes a single image as an input and performs a random transformation
@@ -37,12 +67,14 @@ class ImageGenerator:
 
         return img
 
-    def class_name(self, x):
+    def class_name(self, int_label):
         # This function returns the class name for a specific input
         #TODO: implement class name function
-        return
+        return self.class_dict[int_label]
+
     def show(self):
         # In order to verify that the generator creates batches as required, this functions calls next to get a
         # batch of images and labels and visualizes it.
         #TODO: implement show method
+        pass
 
