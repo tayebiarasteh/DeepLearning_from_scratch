fa6adb20c9320df924d184a6b8a5fe9dd274f40f
eb88ibyw <soroosh.arasteh@fau.de>
2019-10-28 22:34:56 +0100

    Relocating

--- Assignment 0/main.py
+++ Assignment 0/main.py
@@ -6,25 +6,25 @@ import pdb
 '''
 Checker
 '''
-# sample_checker = Checker(800,100)
-# sample_checker.draw()
-# sample_checker.show()
+sample_checker = Checker(800,100)
+sample_checker.draw()
+sample_checker.show()
 
 '''
 Spectrum
 '''
 
-# sample_spectrum = Spectrum(256)
-# sample_spectrum.draw()
-# sample_spectrum.show()
+sample_spectrum = Spectrum(256)
+sample_spectrum.draw()
+sample_spectrum.show()
 
 '''
 Circle
 '''
 
-# sample_circle = Circle(250, 50, (130,70))
-# sample_circle.draw()
-# sample_circle.show()
+sample_circle = Circle(250, 50, (130,70))
+sample_circle.draw()
+sample_circle.show()
 
 #########################
 '''
