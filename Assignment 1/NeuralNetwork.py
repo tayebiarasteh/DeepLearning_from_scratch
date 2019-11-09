e9a30d90ece6dd1bfa9abdd0c392b6775e3ad40f
eb88ibyw <soroosh.arasteh@fau.de>
2019-11-09 19:08:26 +0100

    All the tests passed!

--- Assignment 1/NeuralNetwork.py
+++ Assignment 1/NeuralNetwork.py
@@ -28,17 +28,6 @@ class NeuralNetwork:
         self.prediction = input
         loss = self.loss_layer.forward(input, self.label_tensor)
         self.loss.append(loss)
-        print(loss)
-        # self.input_tensor = self.data_layer[0]
-        # self.label_tensor = self.data_layer[1]
-        # fcl = FullyConnected.FullyConnected(self.input_tensor.shape[0], self.input_tensor.shape[1])
-        # self.input_tensor = fcl.forward(self.input_tensor)
-        # relu = ReLU.ReLU()
-        # self.input_tensor = relu.forward(self.input_tensor)
-        # softmax = SoftMax.SoftMax()
-        # self.input_tensor = softmax.forward(self.input_tensor)
-        # loss = Loss.CrossEntropyLoss()
-        # self.input_tensor = loss.forward(self.input_tensor, self.label_tensor)
 
         return loss
 
@@ -46,32 +35,30 @@ class NeuralNetwork:
     def backward(self):
         error_tensor = self.loss_layer.backward(self.label_tensor)
         for layer in reversed(self.layers):
-            print(layer)
             error_tensor = layer.backward(error_tensor)
-
-        # loss = Loss.CrossEntropyLoss()
-        # self.error_tensor = loss.backward(self.label_tensor)
-        # softmax = SoftMax.SoftMax()
-        # self.error_tensor = softmax.backward(self.error_tensor)
-        # relu = ReLU.ReLU()
-        # self.error_tensor = relu.backward(self.error_tensor)
-        # fcl = FullyConnected.FullyConnected(self.input_tensor.shape[0], self.input_tensor.shape[1])
-        # self.error_tensor = fcl.backward(self.error_tensor)
-
-        #return self.error_tensor
 
 
     def append_trainable_layer(self, layer):
+        # deep copy??? not working with np.copy(self.optimizer)
+        layer.optimizer = self.optimizer
         self.layers.append(layer)
 
 
     def train(self,iterations):
         for i in range(iterations):
-            self.forward()
+            loss = self.forward()
+            if (i+1)%50 == 0:
+                print("training iteration",  str(i+1) + ":", 'loss =', loss)
             self.backward()
 
+
     def test(self,input_tensor):
-        self.forward()
-        return self.prediction
+        '''
+        propagates the input tensor through the network
+        and returns the prediction of the last layer.
+        '''
+        for layer in self.layers:
+            input_tensor = layer.forward(input_tensor)
+        return input_tensor
 
 
