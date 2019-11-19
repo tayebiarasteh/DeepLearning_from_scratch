39014724c6c1a0ae7487982e38b3c92888e700a1
eb88ibyw <soroosh.arasteh@fau.de>
2019-11-19 16:56:11 +0100

    Initializers.py done and passed the tests

--- Assignment 2/Layers/Initializers.py
+++ Assignment 2/Layers/Initializers.py
@@ -4,18 +4,15 @@ import numpy as np
 class Constant:
     def __init__(self, weight_initialization=0.1):
         '''
-        :param weight_initialization: determines the constant value used for weight initialization.
+        :param weight_initialization: the constant value used for weight initialization.
         '''
         self.weight_initialization = weight_initialization
-
 
     def initialize(self, weights_shape, fan_in, fan_out):
         ''''
         returns an initialized tensor of the desired shape.
         '''
-
-        pass
-
+        return (np.zeros(fan_in, fan_out) + self.weight_initialization)
 
 
 
@@ -23,13 +20,11 @@ class UniformRandom:
     def __init__(self):
         pass
 
-
     def initialize(self, weights_shape, fan_in, fan_out):
         ''''
         returns an initialized tensor of the desired shape.
         '''
-
-        pass
+        return np.random.rand(fan_in, fan_out)
 
 
 
@@ -37,13 +32,11 @@ class Xavier:
     def __init__(self):
         pass
 
-
     def initialize(self, weights_shape, fan_in, fan_out):
         ''''
         returns an initialized tensor of the desired shape.
         '''
-
-        pass
+        return np.random.normal(0, (2 / (fan_out + fan_in))**(1/2), weights_shape)
 
 
 
@@ -51,11 +44,9 @@ class He:
     def __init__(self):
         pass
 
-
     def initialize(self, weights_shape, fan_in, fan_out):
         ''''
         returns an initialized tensor of the desired shape.
         '''
-
-        pass
+        return np.random.normal(0, (2 / fan_in)**(1/2), weights_shape)
 
