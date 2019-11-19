6b8cde01ec47ea7349fb03e09bb8039b513e5c50
eb88ibyw <soroosh.arasteh@fau.de>
2019-11-19 22:11:30 +0100

    Flatten() completely done!

--- Assignment 2/Layers/Flatten.py
+++ Assignment 2/Layers/Flatten.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pdb
 
 
 class Flatten:
@@ -9,12 +8,17 @@ class Flatten:
 
     def forward(self, input_tensor):
         '''reshapes and returns the input tensor.'''
+
+        #saves the input tensor dimensions
+        self.input_tensor_dim = (input_tensor.shape[0], input_tensor.shape[1], input_tensor.shape[2], input_tensor.shape[3])
         result = np.zeros((input_tensor.shape[0], input_tensor.shape[1]*input_tensor.shape[2]*input_tensor.shape[3]))
         for i in range(input_tensor.shape[0]):
+            #first dimension is the batches.
             result[i] = input_tensor[i].flatten()
         return result
 
 
 
     def backward(self, error_tensor):
-        pass
+        '''reshapes back the input tensor.'''
+        return error_tensor.reshape(self.input_tensor_dim)
