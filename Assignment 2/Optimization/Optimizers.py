9b72d5dba0e252f2df3e1df10b1d51a7ddd8b5ec
eb88ibyw <soroosh.arasteh@fau.de>
2019-11-19 22:03:38 +0100

    Optimizers initial commit

--- Assignment 2/Optimization/Optimizers.py
+++ Assignment 2/Optimization/Optimizers.py
@@ -7,12 +7,32 @@ class Sgd:
 
     def calculate_update(self, weight_tensor, gradient_tensor):
         '''
-
-        :param weight_tensor:
+        :param weight_tensor: previous weight tensor
         :param gradient_tensor: dL/dW
         :return: the updated weights according to the basic gradient descent update scheme
         '''
         return weight_tensor - self.learning_rate*gradient_tensor
 
 
+class SgdWithMomentum:
+    def __init__(self, learning_rate, momentum_rate):
+        self.learning_rate = learning_rate
+        self.momentum_rate = momentum_rate
 
+
+    def calculate_update(self, weight_tensor, gradient_tensor):
+        # v_k_previous = self.learning_rate*gradient_tensor
+        v_k = self.momentum_rate - self.learning_rate*gradient_tensor
+        return weight_tensor + v_k
+
+
+
+class Adam:
+    def __init__(self, learning_rate, mu, rho):
+        self.learning_rate = learning_rate
+        self.mu = mu
+        self.rho = rho
+
+
+    def calculate_update(self, weight_tensor, gradient_tensor):
+        pass
