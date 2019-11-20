603747fdc37a065d3da9585080d7ac2f5d1afabd
eb88ibyw <soroosh.arasteh@fau.de>
2019-11-20 19:49:06 +0100

    test_forward_size() & test_forward() of "Conv" passed

--- Assignment 2/Layers/Conv.py
+++ Assignment 2/Layers/Conv.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import signal
 import pdb
 
 
@@ -8,19 +9,39 @@ class Conv:
         '''
         :param stride_shape: can be a single value or a tuple. The latter allows for different strides
         in the spatial dimensions.
-        :param convolution_shape:
+
+        :param convolution_shape: determines whether this objects provides a 1D or a 2D convolution layer.
+        For 1D, it has the shape [c, m], whereas for 2D, it has the shape [c, m, n],
+        where c represents the number of input channels, and m, n represent the spacial extent of the filter kernel.
+
         :param num_kernels: an integer value
         '''
         self.stride_shape = stride_shape
         self.convolution_shape = convolution_shape
         self.num_kernels = num_kernels
+
+        # only for 2D image for now
+        self.weights = np.random.rand(num_kernels, convolution_shape[0], convolution_shape[1], convolution_shape[2])
+        self.bias = 0
         self._optimizer = None
         self._gradient_weights = None
         self._gradient_bias = None
 
 
     def forward(self, input_tensor):
-        pass
+        result = np.zeros((input_tensor.shape[0], self.num_kernels, input_tensor.shape[2], input_tensor.shape[3]))
+
+        #loop over batches
+        for i in range(input_tensor.shape[0]):
+
+            # loop over different kernels
+            for ii in range(self.weights.shape[0]):
+
+                # correlation gives an output with channels, we should sum it over channels to get a 2d one-channel image
+                temp1 = signal.correlate(input_tensor[i], self.weights[ii], mode='same').sum(axis=0)
+                result[i,ii]= temp1
+        return result
+
 
 
 
